3c2ea747362be8957d55d1e45c15a71a7f12018e
JavierLiveratoAppsolution <liverato@appsolution.co>
2019-05-07 01:23:03 -0500

    assasas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var app         = express();
 var os = require('os')
-
+var networkInterfaces = os.networkInterfaces( );
 const PORT = process.env.PORT || 3000
 app.set('port', (process.env.PORT || 5000));
 
@@ -19,9 +19,17 @@ var router = require('./src/router');
 app.use('/', router);
 
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 app.listen(app.get('port'), function(){
+	for (var devName in networkInterfaces) {
+		var iface = networkInterfaces[devName];
+		for (var i = 0; i < iface.length; i++) {
+			var alias = iface[i];
+			if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+				console.log(alias.address);
+		}
+	}
 	console.log('server running on '+os.hostname()+"--"+PORT);
 });
